b4c0871b3df7f9748c1d7ed5fb4fee7ec2c07ec9
CelticHarp <amalia.debian@gmail.com>
2023-01-14 01:59:51 +0200

    tests

--- javascript_package/index.js
+++ javascript_package/index.js
@@ -3,26 +3,30 @@ var ref = require('ref-napi')
 
 // typedefs
 var myobj = ref.types.void // we don't know what the layout of "myobj" looks like
-var myobjPtr = ref.refType(myobj);
+var data = ref.refType(myobj);
 
-var brainflow = ffi.Library('../compiled/Release/DataHandler.dll', {
-  'get_version_data_handler': [ 'string', [ 'int', 'int', 'int' ] ],
-  'set_log_level_data_handler': [ 'int', [ 'int' ] ],
-})
-// var brainflow = ffi.Library('../python_package/brainflow/lib/MuseLib.dll', {
-//   'get_version': []
+// var DataFilter = ffi.Library('../compiled/Release/DataHandler.dll', {
+//   'get_version_data_handler': [ 'string', [ 'int', 'int', 'int' ] ],
+//   'set_log_level_data_handler': [ 'int', [ 'int' ] ],
+//   'detect_peaks_z_score': [ 'int', [ data, 'int' , 'int','double','double','double' ] ],
 // })
 
+var BoardShim  = ffi.Library('../compiled/Release/BoardController.lib', {
+  //'get_version': ['string' , []],
+  'set_log_level': ['void', ['int']]
+})
+
+
+var output = BoardShim.set_log_level(1)
+console.log('Your output: ' + output)
 
 // if (process.argv.length < 3) {
 //   console.log('Arguments: ' + process.argv[0] + ' ' + process.argv[1] + ' <max>')
 //   process.exit()
 // }
 
-//var output = libfactorial.factorial(parseInt(process.argv[2]))
-// exports.set_log_level_data_handler = brainflow.set_log_level_data_handler;
-var output = brainflow.set_log_level_data_handler(1)
-console.log('Your output: ' + output)
+// var output = DataFilter.get_version_data_handler(1,2,3)
+// console.log('Your output: ' + output)
 
-var output = brainflow.get_version_data_handler(1,2,3)
-console.log('Your output: ' + output)
+// var output = DataFilter.detect_peaks_z_score(null,1, 1, 3, 4, 5)
+// console.log('Your output: ' + output)
