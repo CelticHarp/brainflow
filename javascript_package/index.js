6112e6412f15a9496fbad5827b7974edfa47f375
CelticHarp <amalia.debian@gmail.com>
2023-01-14 00:43:35 +0200

    added ref-napi

--- javascript_package/index.js
+++ javascript_package/index.js
@@ -1,7 +1,12 @@
 var ffi = require('ffi-napi')
+var ref = require('ref-napi')
+
+// typedefs
+var myobj = ref.types.void // we don't know what the layout of "myobj" looks like
+var myobjPtr = ref.refType(myobj);
 
 var brainflow = ffi.Library('../compiled/Release/DataHandler.dll', {
-  //'get_version_data_handler': [ 'string', [ 'string', 'int', 'int' ] ],
+  'get_version_data_handler': [ 'string', [ 'int', 'int', 'int' ] ],
   'set_log_level_data_handler': [ 'int', [ 'int' ] ],
 })
 // var brainflow = ffi.Library('../python_package/brainflow/lib/MuseLib.dll', {
@@ -15,6 +20,9 @@ var brainflow = ffi.Library('../compiled/Release/DataHandler.dll', {
 // }
 
 //var output = libfactorial.factorial(parseInt(process.argv[2]))
+// exports.set_log_level_data_handler = brainflow.set_log_level_data_handler;
 var output = brainflow.set_log_level_data_handler(1)
+console.log('Your output: ' + output)
 
+var output = brainflow.get_version_data_handler(1,2,3)
 console.log('Your output: ' + output)
